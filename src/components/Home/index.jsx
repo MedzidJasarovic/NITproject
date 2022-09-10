955248b9ed92f5c2aa5fabec45dbe66508c6ebf4
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2022-09-10 12:45:01 +0200

    ceft

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,7 @@ import style from "./index.module.css";
 
 const Home = () => {
   const [articles, setArticles] = useState([]);
+
   //"/v2/top-headlines?country=us&apiKey=a3ab634aacf04e55b898475a2c594dd6"
 
   const getNews = () => {
@@ -24,14 +25,125 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      {articles.map((article) => {
-        return (
-          <div className={style.kartica}>
-            <p className={style.deskripcijaa}>{article.description}</p>
-            <p className={style.autor}>{article.author}</p>
-          </div>
-        );
+    <div className={style.homediv}>
+      {articles.map((article, index) => {
+        if (index >= 15) {
+          if (index - 15 > 2) {
+            return (
+              <div className={style.kartica}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div>
+                  <img
+                    className={style.slike}
+                    src={article.urlToImage}
+                    alt="nema"
+                  />
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div className={style.karticaManja}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div>
+                  <img
+                    className={style.slike}
+                    src={article.urlToImage}
+                    alt="nema"
+                  />
+                </div>
+              </div>
+            );
+          }
+        } else if (index >= 10) {
+          if (index - 10 > 2) {
+            return (
+              <div className={style.kartica}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div>
+                  <img
+                    className={style.slike}
+                    src={article.urlToImage}
+                    alt="nema"
+                  />
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div className={style.karticaManja}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div>
+                  <img
+                    className={style.slike}
+                    src={article.urlToImage}
+                    alt="nema"
+                  />
+                </div>
+              </div>
+            );
+          }
+        } else if (index >= 5) {
+          if (index - 5 > 2) {
+            return (
+              <div className={style.kartica}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div>
+                  <img
+                    className={style.slike}
+                    src={article.urlToImage}
+                    alt="nema"
+                  />
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div className={style.karticaManja}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div>
+                  <img
+                    className={style.slike}
+                    src={article.urlToImage}
+                    alt="nema"
+                  />
+                </div>
+              </div>
+            );
+          }
+        } else if (index >= 5) {
+          if (index > 2) {
+            return (
+              <div className={style.kartica}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div>
+                  <img
+                    className={style.slike}
+                    src={article.urlToImage}
+                    alt="nema"
+                  />
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div className={style.karticaManja}>
+                <p className={style.deskripcijaa}>{article.description}</p>
+                <p className={style.autor}>{article.author}</p>
+                <div className={style.slike}>
+                  <img src={article.urlToImage} alt="nema" />
+                </div>
+              </div>
+            );
+          }
+        }
       })}
     </div>
   );
